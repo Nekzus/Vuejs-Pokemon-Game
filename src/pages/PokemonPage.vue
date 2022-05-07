218ae8b07d08ed20413152ace3406cab2d20db1f
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-05-07 17:36:29 -0300

    Funcionalidad componente Picture

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+
+  <PokemonPicture :pokemonId="85" :showPokemon="true" />
+
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions";
+
+export default {
+  name: "PokemonPage",
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
+</script>
